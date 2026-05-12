f3e730cbcd86d7f4351d8f1bc0ce3ea9766f9606
Vapankung <pacharee.o0606@gmail.com>
2026-05-12 18:22:19 +0700

    Fix GitHub Pages deployment

--- simulator/vite.config.js
+++ simulator/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Vapankung/Rubnong_workshop.git",
+  base: "/Rubnong_workshop/",
 });
